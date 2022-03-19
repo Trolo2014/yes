5047be6f6edd50ef130c62304324010ca0d1f30d
Trolo2014 <98257633+Trolo2014@users.noreply.github.com>
2022-03-19 14:48:55 +0100

    Add files via upload

---
+++ ChildHoodGame.lua
@@ -0,0 +1,129 @@
+repeat wait() until game:IsLoaded()
+if game.PlaceId == 258986981 then
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("OG dragon ball game", "Synapse")
+local Tab = Window:NewTab("Main")
+local MainSection = Tab:NewSection("Main")
+MainSection:NewToggle("Auto Exp i gues", "ToggleInfo", function(state)
+    if state then
+        _G.s = true
+while _G.s do
+    wait()
+local args = {
+    [1] = "xSTR",
+    [2] = 1
+}
+
+game:GetService("ReplicatedStorage").FilteringEnabled.Events.AddStats:FireServer(unpack(args))
+
+local args = {
+    [1] = "MeteorSmash"
+}
+
+workspace.Packets.PacketEvent_trolo2014:FireServer(unpack(args))
+
+local args = {
+    [1] = "Combat"
+}
+
+workspace.Packets.PacketEvent_trolo2014:FireServer(unpack(args))
+
+
+end
+    else
+        _G.s = false
+    end
+end)
+
+
+MainSection:NewToggle("auto wishing power", "sex", function(state)
+    if state then
+        _G.Wish = true
+while _G.Wish do
+    wait(1)
+    spawn(function()
+if game:GetService("Players").trolo2014.PlayerGui.GameplayGui.Wishes.Summon.Visible == true then
+local args = {[1] = "Summon"}
+game:GetService("ReplicatedStorage").FilteringEnabled.Events.Shenron:FireServer(unpack(args))
+local args = {[1] = "Power"}
+game:GetService("ReplicatedStorage").FilteringEnabled.Events.Wish:FireServer(unpack(args))
+end
+end)
+end
+    else
+      _G.Wish = false  
+    end
+end)
+
+MainSection:NewToggle("Dragon Balls Collecting", "sex", function(state)
+    if state then
+game:GetService("Players").trolo2014.PlayerGui.GameplayGui.Notice:Destroy()
+_G.DragonBalls = true
+while _G.DragonBalls do
+wait(0.2)
+spawn(function()
+for i,v in pairs(game:GetService("Workspace").ItemDrop:GetDescendants()) do
+if v:IsA("ClickDetector") then
+fireclickdetector(v)
+end
+end
+end)
+end
+    else
+        _G.DragonBalls = false
+    end
+end)
+MainSection:NewToggle("Autofarm LV 4 HTC Dummy", "sex", function(state)
+    if state then
+_G.Loopbring = true
+       while _G.Loopbring do
+       wait()
+--LV 4 HTC Dummy
+for i,v in pairs(game.Workspace.Enemies:GetChildren()) do
+if v:IsA("Model") and  string.find(v.Name, "LV 4 HTC Dummy")  and v:FindFirstChild("HumanoidRootPart") then
+v.HumanoidRootPart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame * CFrame.new(0, 1, 0) + game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame.lookVector * 0
+v.HumanoidRootPart.Anchored = true
+end
+end
+end
+    else
+        _G.Loopbring = false
+    end
+end)
+
+
+MainSection:NewToggle("Autofarm Zenos Dummys", "sex", function(state)
+    if state then
+_G.Loopbring = true
+       while _G.Loopbring do
+       wait()
+--LV 4 HTC Dummy
+for i,v in pairs(game.Workspace.Enemies:GetChildren()) do
+if v:IsA("Model") and  string.find(v.Name, "Zeno Dummy")  and v:FindFirstChild("HumanoidRootPart") then
+v.HumanoidRootPart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame * CFrame.new(0, 1, 0) + game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame.lookVector * 0
+v.HumanoidRootPart.Anchored = true
+end
+end
+end
+    else
+        _G.Loopbring = false
+    end
+end)
+
+
+MainSection:NewToggle("Auto Usage ki", "sex", function(state)
+    if state then
+        _G.Soda = true
+        while _G.Soda do
+            wait()
+            if game:GetService("Players").trolo2014.PlayerGui.GameplayGui.Status.Ki.Label.Text == '50/1300' then
+    game:GetService("ReplicatedStorage").FilteringEnabled.Events.zSoda:FireServer()
+    end
+    end
+    else
+     _G.Soda = false
+    end
+end)
+end
+
+
